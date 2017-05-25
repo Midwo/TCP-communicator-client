bb62761b93b3f689e6ede87e994f4614034f2cf3
Midwo <michal.dwojak92@gmail.com>
2017-05-25 19:54:00 +0200

    Add project files.

---
+++ KomunikatorKlient/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace KomunikatorKlient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 454);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(337, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Wyślij";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(7, 389);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(337, 59);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyUp);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+            this.webBrowser1.Location = new System.Drawing.Point(7, 163);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(337, 192);
+            this.webBrowser1.TabIndex = 8;
+            this.webBrowser1.WebBrowserShortcutsEnabled = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(337, 155);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Konfiguracja Serwera";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(2, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(322, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(204, 95);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Port:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Adres:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(2, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(322, 69);
+            this.listBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(42, 94);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "127.0.0.1";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(7, 360);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(25, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "B";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(38, 360);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(25, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "I";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(351, 484);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MD komunikator klient";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ KomunikatorKlient/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KomunikatorKlient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            webBrowser1.Navigate("about:blank");
+            webBrowser1.Document.Write("<html><head><style>body,table { font-size: 12pt; font - family: Verdana; margin: 3px 3px 3px 3px; font - color: black; }</style></head><body width =\"" + (webBrowser1.ClientSize.Width - 20).ToString() + "\">");
+        }
+
+        private TcpClient klient;
+        private BinaryWriter pisanie;
+        private BinaryReader czytanie;
+        private string serwerIP = "127.0.0.1";
+        private int PozycjaKursora;
+        private bool polaczeniaAktywne;
+
+
+        delegate void SetTextCallBack(string tekst);
+        delegate void SetScrollCallBack();
+        private void SetText(string tekst)
+        {
+            if (listBox1.InvokeRequired)
+            {
+                SetTextCallBack f = new SetTextCallBack(SetText);
+                this.Invoke(f, new object[] { tekst });
+            }
+            else
+            {
+                this.listBox1.Items.Add(tekst);
+            }
+        }
+        private void SetTextHTML(string tekst)
+        {
+            if (webBrowser1.InvokeRequired)
+            {
+                SetTextCallBack f = new SetTextCallBack(SetTextHTML);
+                this.Invoke(f, new object[] { tekst });
+            }
+            else
+            {
+                this.webBrowser1.Document.Write(tekst);
+            }
+        }
+        private void SetScroll()
+        {
+            if (webBrowser1.InvokeRequired)
+            {
+                SetScrollCallBack s = new SetScrollCallBack(SetScroll);
+                this.Invoke(s);
+            }
+            else
+            {
+                webBrowser1.Document.Window.ScrollTo(0, webBrowser1.Document.Body.ScrollRectangle.Height);
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                klient = new TcpClient(serwerIP, (int)numericUpDown1.Value);
+                NetworkStream ns = klient.GetStream();
+                czytanie = new BinaryReader(ns);
+                pisanie = new BinaryWriter(ns);
+                pisanie.Write("###HI###");
+                this.SetText("Autoryzacja ...");
+                polaczeniaAktywne = true;
+                backgroundWorker2.RunWorkerAsync();
+            }
+            catch
+            {
+                this.SetText("Nie można nawiązać połączenia");
+                polaczeniaAktywne = false;
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            SetText("Autoryzacja zakończona");
+            string wiadomosc;
+            try
+            {
+                while ((wiadomosc = czytanie.ReadString()) != "###BYE###")
+                {
+                    encryptDecrypt decrypt = new encryptDecrypt();
+                    string getstring = decrypt.decrypt(wiadomosc);
+                    WpiszTekst("ktoś", getstring);
+                }
+                SetText("Połączenie przerwane");
+                polaczeniaAktywne = false;
+                klient.Close();
+            }
+            catch
+            {
+                SetText("Połączenie z serwerem zostało przerwane");
+                polaczeniaAktywne = false;
+                klient.Close();
+            }
+        }
+
+        private void WpiszTekst(string kto, string wiadomosc)
+        {
+            SetTextHTML("<table><tr><td width=\"10%\"><b>" + kto + "</b></td><td width =\"90%\">(" + DateTime.Now.ToShortTimeString() + "):</td></tr>");
+            SetTextHTML("<tr><td colspan=2>" + wiadomosc + "</td></tr></table>");
+            SetTextHTML("<hr>");
+            SetScroll();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (polaczeniaAktywne == false)
+            {
+                backgroundWorker1.RunWorkerAsync();
+                webBrowser1.Navigate("about:blank");
+            }
+            else
+            {
+                polaczeniaAktywne = false;
+                if (klient != null)
+                {
+                    pisanie.Write("###BYE###");
+                    klient.Close();
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+               WpiszTekst("ja", textBox1.Text);
+            if (polaczeniaAktywne)
+            {
+                encryptDecrypt encrypt = new encryptDecrypt();
+                string encryptString = encrypt.encrypt(textBox1.Text);
+                pisanie.Write(encryptString);
+            }
+            textBox1.Text = "";
+            SetScroll();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            WprowadzTag("<b></b>");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            WprowadzTag("<i></i>");
+        }
+
+        private void WprowadzTag(string tag)
+        {
+            try
+            {
+                string kod = textBox1.Text;
+                textBox1.Text = kod.Insert(PozycjaKursora, tag);
+                textBox1.Focus();
+                if (tag == "<br>" || tag == "<hr>")
+                {
+                    textBox1.Select(PozycjaKursora + tag.Length, 0);
+                    PozycjaKursora += tag.Length;
+                }
+                else
+                {
+                    textBox1.Select(PozycjaKursora + tag.Length / 2, 0);
+                    PozycjaKursora += tag.Length / 2;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nie zaznaczono pola: " + ex.Message, "Error");
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+
+                button2_Click(sender, null);
+            }
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            PozycjaKursora = textBox1.SelectionStart;
+        }
+    }
+
+}
